b35e5c382edef44c65dcdf63e17f7fd2fac71c09
free20064u <free20064u@gmail.com>
2021-04-04 01:03:58 +0000

    initial commit

--- index.php
+++ index.php
@@ -1,100 +1,145 @@
 <?php 
-	require_once 'partials/connection.php';
-	include_once 'partials/header.php';
+include("path.php");
+include(ROOT_PATH . "/app/controllers/topics.php");
 
-	# Retrieveing information from the post tabel.
-	$stmt = $connect -> query("SELECT * FROM `post` ORDER BY `post_id` DESC LIMIT 3");
-	$rows = $stmt -> fetchAll();
-	
-	$stmt1 = $connect -> query("SELECT * FROM `post` ORDER BY `post_id` DESC LIMIT 3,3");
-	$rows1 = $stmt1 -> fetchAll();
+$posts = array();
+$postsTitle = 'Recent Posts';
 
-
-?>
-	<!-- Features -->
-	<div id="features-wrapper">
-		<div class="container">
-		<?php echo '<h2>Latest</h2>' ?>
-			<div class="row">
-			<?php $a = 5;
-				foreach ($rows as $row){
-					echo '<div class="col-4 col-12-medium">
-							<!-- Box -->
-							<section class="box feature">
-								<a href="#" class="image featured"><img src="images/pic' . $a . '.jpg" alt="" /></a>
-								<div class="inner">
-									<header>
-										<h2>' . $row['title'] . '</h2>
-										<p>'. $row['date_posted'] .'</p>
-									</header>			
-								</div>
-							</section>
-						</div>';
-						$a = $a + 1;
-					}?>
-			</div>
-		</div>
-	</div>
-
-	<!-- Main -->
-	<div id="main-wrapper">
-		<div class="container">
-			<div class="row gtr-200">
-				<div class="col-4 col-12-medium">
-					<!-- Sidebar -->
-					<div id="sidebar">
-						<section class="widget thumbnails">
-							<h3>Interesting stuff</h3>
-							<div class="grid">
-								<div class="row gtr-50">
-									<div class="col-6"><a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a></div>
-									<div class="col-6"><a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a></div>
-									<div class="col-6"><a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a></div>
-									<div class="col-6"><a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a></div>
-								</div>
-							</div>
-							<a href="#" class="button icon fa-file-alt">More</a>
-						</section>
-					</div>
-				</div>
-				<div class="col-8 col-12-medium imp-medium">
-					<!-- Content -->
-					<div id="content">
-						<section class="last">
-							<h2>More news</h2>
-							<div class="row">
-							<?php  $b = $a;
-								foreach ($rows1 as $row1){
-									echo '<div class="col-12 col-12-medium">
-											<!-- Box -->
-											<section class="box feature">
-												<div class="inner" style="background-color: #2abcfe">
-												<div class="row">
-													<div class="col-4 col-12-medium">
-														<a href="#" class="image featured"><img src="images/pic' . $b . '.jpg" alt="" /></a>
-													</div>
-													<div class="col-8 col-12-medium">
-														<h2>' . $row1['title'] . '</h2>
-														<p>'. $row1['content'] .'</p>
-													</div>
-												</div>
-															
-												</div>
-											</section>
-										</div>';
-										$b = $b + 1;
-								}
-							?>
-							</div>
-							
-						</section>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php 
-
-	include_once 'partials/footer.php';
+if (isset($_GET['t_id'])) {
+  $posts = getPostsByTopicId($_GET['t_id']);
+  $postsTitle = "You searched for posts under '" . $_GET['name'] . "'";
+} else if (isset($_POST['search-term'])) {
+  $postsTitle = "You searched for '" . $_POST['search-term'] . "'";
+  $posts = searchPosts($_POST['search-term']);
+} else {
+  $posts = getPublishedPosts();
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Blog</title>
+</head>
+
+<body>
+
+  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+  <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
+
+
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    <!-- Post Slider -->
+    <div class="post-slider">
+      <h1 class="slider-title">Trending Posts</h1>
+      <i class="fas fa-chevron-left prev"></i>
+      <i class="fas fa-chevron-right next"></i>
+
+      <div class="post-wrapper">
+
+        <?php foreach ($posts as $post): ?>
+          <div class="post">
+            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="slider-image">
+            <div class="post-info">
+              <h4><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h4>
+              <i class="far fa-user"> <?php echo $post['username']; ?></i>
+              &nbsp;
+              <i class="far fa-calendar"> <?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+            </div>
+          </div>
+        <?php endforeach; ?>
+
+
+      </div>
+
+    </div>
+    <!-- // Post Slider -->
+
+    <!-- Content -->
+    <div class="content clearfix">
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <h1 class="recent-post-title"><?php echo $postsTitle ?></h1>
+
+        <?php foreach ($posts as $post): ?>
+          <div class="post clearfix">
+            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="post-image">
+            <div class="post-preview">
+              <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+              <i class="far fa-user"> <?php echo $post['username']; ?></i>
+              &nbsp;
+              <i class="far fa-calendar"> <?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+              <p class="preview-text">
+                <?php echo html_entity_decode(substr($post['body'], 0, 150) . '...'); ?>
+              </p>
+              <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
+            </div>
+          </div>    
+        <?php endforeach; ?>
+        
+
+
+      </div>
+      <!-- // Main Content -->
+
+      <div class="sidebar">
+
+        <div class="section search">
+          <h2 class="section-title">Search</h2>
+          <form action="index.php" method="post">
+            <input type="text" name="search-term" class="text-input" placeholder="Search...">
+          </form>
+        </div>
+
+
+        <div class="section topics">
+          <h2 class="section-title">Topics</h2>
+          <ul>
+            <?php foreach ($topics as $key => $topic): ?>
+              <li><a href="<?php echo BASE_URL . '/index.php?t_id=' . $topic['id'] . '&name=' . $topic['name'] ?>"><?php echo $topic['name']; ?></a></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- // Content -->
+
+  </div>
+  <!-- // Page Wrapper -->
+
+  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
--- partials/helper.php
+++
@@ -1,7 +0,0 @@
-<?php 
-function search_user($username){
-    global $connect;
-    $stmt = $connect -> prepare('SELECT `password`, `status` FROM `users` WHERE `username` = (?)');
-    $stmt -> execute(array($username));
-    $rows = $stmt->fetchAll();
-}
